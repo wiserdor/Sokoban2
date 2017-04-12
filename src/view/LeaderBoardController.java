8660d6441a60b7149d1ffc286a1c4e9f4f1504b6
Dor <Dor@Dor-pc>
2017-04-12 21:59:49 +0300

    close button

--- src/view/LeaderBoardController.java
+++ src/view/LeaderBoardController.java
@@ -1,13 +1,16 @@
 package view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LeaderBoardController {
 	@FXML
 	private Button bt1;
 	public void close(){
-		
+		Stage stage = (Stage) bt1.getScene().getWindow();
+	    stage.close();
 	}
 
 }
